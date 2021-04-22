68455664c0d7c3f938dcc5beb1adb0fdbafe8957
gancherj <jrg358@cornell.edu>
2021-04-22 12:20:34 -0400

    did as well

--- Focus.v
+++ Focus.v
@@ -47,3 +47,30 @@ Eval compute in (lower (lift john <| (lift bought |> (only |> focus lunch)))).
 
 Eval compute in (lower ((only |> focus john) <| (lift bought |> lift lunch))).
 
+
+
+(* prop anaphora with focussing *)
+
+Definition unfocus {a b c d : Cat} (x : a || b -- (c[d])) : a || b -- c .
+  refine (fmap _ x).
+  apply (fun '(b, a) => a b).
+Defined.
+                                                              
+Definition did_as_well : (S[DP] >> S) || S -- (DP \ S) :=
+  (fun k '(_, f) => k (fun x => f x)).
+
+Definition gbind {a b c : Cat} (x : a || b -- c) : a || (c >> b) -- c :=
+  fun k => x (fun e => k e e).
+
+Definition reset {a c : Cat} (x : a || S -- S) : c || c -- a := lift (lower x).
+
+
+(* We focus john, and evaluate the first sentence. We call gbind on this, and then consider only the ordinary semantics of the first sentence. *)
+Check (unfocus (gbind (reset (focus john <| (lift bought |> lift lunch))))).
+   
+
+(* Thus we now have a referent of type S[DP]. This can be fed into did_as_well. *)
+Eval compute in (lower ((unfocus (gbind (reset (focus john <| (lift bought |> lift lunch)))))
+         <|
+         (lift and |>
+               (lift mary <| did_as_well)))).
